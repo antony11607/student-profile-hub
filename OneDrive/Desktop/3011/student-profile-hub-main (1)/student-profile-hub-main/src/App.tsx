11a5fc5b8a54e94b6e339abe36b56896e18329d2
antony.110607 <antonyxavierjm@gmail.com>
2025-12-07 08:07:47 -0800

    0712

--- OneDrive/Desktop/3011/student-profile-hub-main (1)/student-profile-hub-main/src/App.tsx
+++ OneDrive/Desktop/3011/student-profile-hub-main (1)/student-profile-hub-main/src/App.tsx
@@ -6,6 +6,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProjectDetail from "./pages/ProjectDetail";
 import NotFound from "./pages/NotFound";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Internships from "./components/Internships";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"; 
+import ScrollToTop from "./components/ScrollToTop";  
 
 const queryClient = new QueryClient();
 
@@ -15,12 +22,19 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop /> 
+        <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/home" element={<Index />} />
           <Route path="/project/:id" element={<ProjectDetail />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/internships" element={<Internships />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
--- OneDrive/Desktop/3011/student-profile-hub-main (1)/student-profile-hub-main/src/pages/Index.tsx
+++ OneDrive/Desktop/3011/student-profile-hub-main (1)/student-profile-hub-main/src/pages/Index.tsx
@@ -17,7 +17,6 @@ const Index = () => {
       <Stats />
       <Internships />
       <Contact />
-      <Footer />
     </main>
   );
 };
